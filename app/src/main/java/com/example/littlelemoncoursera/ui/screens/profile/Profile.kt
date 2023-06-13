03bf0a502d91df9474a22a625e09c3f8af75a6de
kedrakk <keaindrak@gmail.com>
2023-06-13 16:14:42 +0630

    added toast in order list

--- app/src/main/java/com/example/littlelemoncoursera/ui/screens/profile/Profile.kt
+++ app/src/main/java/com/example/littlelemoncoursera/ui/screens/profile/Profile.kt
@@ -1,5 +1,6 @@
 package com.example.littlelemoncoursera.ui.screens.profile
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -26,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -54,6 +56,7 @@ fun ProfileContent(
     var isShowDropdown by remember {
         mutableStateOf(false)
     }
+    val context = LocalContext.current
     val selectedLang: AppLanguage? =
         AppLanguageList.allLanguages.firstOrNull { it.langCode == currentLangCode }
     Scaffold(
@@ -82,7 +85,9 @@ fun ProfileContent(
             SettingItem(
                 iconData = R.drawable.baseline_list_24,
                 label = stringResource(R.string.view_orders_list),
-                onTap = {},
+                onTap = {
+                        Toast.makeText(context,"Coming Soon",Toast.LENGTH_SHORT).show()
+                },
                 onTrailingTap = {}
             )
             SettingItem(
